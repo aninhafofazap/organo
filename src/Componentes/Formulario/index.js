fb1cd9ca87a330ae8a9178ac37e534d8a214c1aa
Ana Victoria <anavictoriagomes1810@gmail.com>
2023-01-23 17:00:03 -0300

    Controlando inputs com Value e onChange

--- src/Componentes/Formulario/index.js
+++ src/Componentes/Formulario/index.js
@@ -2,6 +2,7 @@ import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
+import { useState } from "react";
 
 const Formulario = () => {
   const times = [
@@ -13,9 +14,14 @@ const Formulario = () => {
     "Mobile",
   ];
 
+  const [nome, setNome] = useState("");
+  const [cargo, setCargo] = useState("");
+  const [imagem, setImagem] = useState("");
+  const [time, setTime] = useState("");
+
   const aoSalvar = (evento) => {
     evento.preventDefault();
-    console.log("Form foi submetido");
+    console.log("Form foi submetido =>", nome, cargo, imagem, time);
   };
 
   return (
@@ -26,14 +32,28 @@ const Formulario = () => {
           obrigatorio={true}
           label="Nome"
           placeholder="Digite seu nome"
+          valor={nome}
+          aoAlterado={(valor) => setNome(valor)}
         />
         <CampoTexto
           obrigatorio={true}
           label="Cargo"
           placeholder="Digite seu cargo"
+          valor={cargo}
+          aoAlterado={(valor) => setCargo(valor)}
         />
-        <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
-        <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
+        <CampoTexto
+          label="Imagem"
+          placeholder="Informe o endereço da imagem"
+          valor={imagem}
+          aoAlterado={(valor) => setImagem(valor)}
+        />
+        <ListaSuspensa
+          obrigatorio={true}
+          label="Time"
+          itens={times}
+          aoAlterado={(valor) => setTime(valor)}
+        />
         <Botao>Criar Card</Botao>
       </form>
     </section>

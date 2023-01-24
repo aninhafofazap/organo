4408fb141dabdae82bd14aaaf2c7f3ca44057aeb
Ana Victoria <anavictoriagomes1810@gmail.com>
2023-01-24 17:16:08 -0300

    Criando parte de colaboradores

--- src/Componentes/Time/index.js
+++ src/Componentes/Time/index.js
@@ -1,3 +1,4 @@
+import Colaborador from "../Colaborador";
 import "./Time.css";
 
 const Time = (props) => {
@@ -7,6 +8,15 @@ const Time = (props) => {
 
     <section className="time" style={{ backgroundColor: props.corSecundaria }}>
       <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+      <div className="colaboradores">
+        {props.colaboradores.map((colaborador) => (
+          <Colaborador
+            nome={colaborador.nome}
+            cargo={colaborador.cargo}
+            imagem={colaborador.imagem}
+          />
+        ))}
+      </div>
     </section>
   );
 };
